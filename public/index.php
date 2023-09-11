67e58631ec70b2239c6b346a8e84e9be34dc0e43
EVassar <erlend.vassar@tptlive.ee>
2023-09-11 15:08:52 +0300

    Add controllers

--- public/index.php
+++ public/index.php
@@ -7,21 +7,34 @@ spl_autoload_register(function ($className){
 
 });
 
-use App\Animals\Dog as Doggo;
+use App\Router;
 
-$router = new App\Router();
-var_dump(new Dog());
+require __DIR__. '/../routes.php';
 
+$router = new Router($_SERVER['REQUEST_URI']);
+$match = $router->match();
+if ($match){
+    if(is_callable($match['action'])){
+        call_user_func($match['action']);
+    } else if(is_array($match['action'])){
+        $class = $match['action'][0]; // 'App\Controllers\PublicController'
+        $controller = new $class();
+        $method = $match['action'][1]; //
+        $controller->$method();
+    };
+}else{
+    echo 404;
+}
 
 //var_dump($_SERVER);
-switch($_SERVER['REQUEST_URI']) {
-    case '/':
-        echo 'HomePage';
-        break;
-    case '/about':
-        echo 'About';
-        break;
-    default:
-        echo '404';
-        break;
-}
+//switch($_SERVER['REQUEST_URI']) {
+//    case '/':
+//        echo 'HomePage';
+//        break;
+//    case '/about':
+//        echo 'About';
+//        break;
+//    default:
+//        echo '404';
+//        break;
+//}
---
+++ src/Controllers/PublicController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class PublicController {
+    public function home() {
+        echo 'home page';
+    }
+    public function about() {
+        echo 'about';
+    }
+}
