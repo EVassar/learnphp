525301d5f18e932b3a8e6c607739996e0be20a2f
EVassar <erlend.vassar@tptlive.ee>
2023-09-11 13:50:51 +0300

    Add namespace example

--- public/index.php
+++ public/index.php
@@ -1,5 +1,18 @@
 <?php
 
+spl_autoload_register(function ($className){
+    //var_dump($className);
+    $className = substr($className, 3);
+    require_once __DIR__. "/../src/$className.php";
+
+});
+
+use App\Animals\Dog as Doggo;
+
+$router = new App\Router();
+var_dump(new Dog());
+
+
 //var_dump($_SERVER);
 switch($_SERVER['REQUEST_URI']) {
     case '/':
